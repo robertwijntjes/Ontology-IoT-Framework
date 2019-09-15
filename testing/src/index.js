91750c6dfa1e8481c04a985011be34a4411119b3
Robert Wijntjes <rwijntjes1@hotmail.com>
2019-09-15 13:14:38 +0800

    Began wordnet similarity developement

--- testing/src/index.js
+++ testing/src/index.js
@@ -1,6 +1,2 @@
-const ontology1 = require('../data/ontology1')
-const recursive = require('./utils/recursiveSearch')
-
-const result = recursive("vunerability",ontology1)
-
-console.log({Result:result})
+const similarity = require('string-similarity')
+const wordnet = require('node-wordnet')
