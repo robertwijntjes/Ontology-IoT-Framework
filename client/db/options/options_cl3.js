a3d04746a649a0a9d73e00e0948f8c0b953a6274
Robert Wijntjes <rwijntjes1@hotmail.com>
2019-09-27 12:31:55 +0800

    Created Options for new ontologies.

---
+++ client/db/options/options_cl3.js
@@ -0,0 +1,30 @@
+const recursive = require('../../src/utils/TreeSearch/recursiveSearch')
+const parent_child = require('../../src/utils/single ontology cal/parent-child')
+const distance_center = require('../../src/utils/single ontology cal/distance-center')
+const SimCheck = require('../../src/utils/dual ontology/SimCheck')
+const {antonyms_list,antonyms_amount,antonyms_array} = require('../../src/utils/single ontology cal/antonyms')
+const {synonyms_list,synonyms_amount,synonyms_array} = require('../../src/utils/single ontology cal/synonyms')
+const {attributes_list,attributes_amount,attributes_array} = require('../../src/utils/single ontology cal/attributes')
+// Utilities
+
+const ontology_3 = require('../data/ontology3')
+// Ontology Import
+
+const option_1 = {
+
+}
+
+const option_2 = {
+    
+}
+
+const option_3 = {
+    
+}
+
+
+module.exports = {
+    option_1,
+    option_2,
+    option_3
+}
