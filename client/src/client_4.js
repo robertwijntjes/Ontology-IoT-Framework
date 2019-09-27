e37f72eedb251fc7bff3f5ec4bd89a4c50f7f4f7
Robert Wijntjes <rwijntjes1@hotmail.com>
2019-09-27 16:03:31 +0800

    Created the two extra clients exe

--- client/src/client_4.js
+++ client/src/client_4.js
@@ -2,7 +2,7 @@ const request = require('request')
 const rp = require('request-promise')
 // Packages
 
-const {option_1,option_2,option_3} = require('../db/options/options_cl1')
+const {option_1,option_2,option_3} = require('../db/options/options_cl4')
 // System options
 
 async function RequestLineExecute() {
@@ -46,19 +46,3 @@ RequestLineExecute()
 
 
 
-
-
-
-
-/*
-@@@@ Client @@@@
-1. Amount of children the node has:                     parent_child(recursive("equipment",ontology_1))
-2. The depth of the node:                               distance_center(recursive("equipment",ontology_1)) 
-3. Synonyms Sim-Calculation                             synonyms(recursive("vulnerability",ontology_1))
-4. Antonym Sim-Calculation (opposite-of):               antonyms(recursive("vulnerability",ontology_1))
-5. Check Node Attributes:                               attributes(recursive("vulnerability",ontology_1))
-
-@@@@ Server @@@@
-1. Similarity Calculation:                              **ping-server**
-
-*/
